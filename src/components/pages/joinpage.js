95d976b3d2471fbcb92d3cb1792e2332c2b4f154
Marcus Östling <marcus.ftw94@gmail.com>
2018-05-16 09:18:44 +0200

    add content to join page

--- src/components/pages/joinpage.js
+++ src/components/pages/joinpage.js
@@ -1,13 +1,34 @@
 import React, { Component } from 'react';
 
-import './startpage.scss';
+import './joinpage.scss';
+import '../../styles/form.scss';
 
 class JoinPage extends Component {
   render() {
     return (
       <div className="JoinPage">
         <h1>Join</h1>
-        
+        <p>
+        Everyone are free to join us for any of the races. However, if you are
+        interested in join our team you can send us an application.
+        </p>
+        <h2>Application</h2>
+        <form>
+          <input type="text" placeholder="Firstname" />
+          <input type="text" placeholder="Lastname" />
+          <input type="text" placeholder="Email" />
+          <textarea placeholder="Write something about yourself!" />
+          <input type="submit" value="Submit" />
+        </form>
+
+        <h2>Newsletter</h2>
+        <p>
+          Get updates about upcoming events.
+        </p>
+        <form>
+          <input type="text" placeholder="Email" />
+          <input type="submit" value="Submit" />
+        </form>
       </div>
     );
   }
