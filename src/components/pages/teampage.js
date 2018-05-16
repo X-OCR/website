ad3cfe0bafbdb6b1b58da1c5b42ed74e595b9ba3
Marcus Östling <marcus.ftw94@gmail.com>
2018-05-16 11:24:31 +0200

    style teampage for images

--- src/components/pages/teampage.js
+++ src/components/pages/teampage.js
@@ -3,22 +3,49 @@ import React, { Component } from 'react';
 import './teampage.scss';
 
 class TeamPage extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      team: { // Tis sh
+        leader: ['Marcus Östling'],
+        developers: ['Ellinor Westerberg', 'Marcus Östling','Gustav Schneider'],
+        eventers: ['Ellinor Westerberg', 'Carl Dath', 'Joakim Lilja'],
+        hangarounds: ['Ellinor Westerberg', 'Robert Berggren', 'Lars Lundin', 'Tomas Möre']
+      }
+    }
+  }
+
+  mapToList = (list) => {
+    return list.map((name) => 
+      <li>{name}</li>
+    );
+  }
+
   render() {
     return (
       <div className="TeamPage">
         <h1>The team</h1>
+        <br/>
+
         <h2>Leader, Boss, MVP, Master, Sensei</h2>
-        Marcus Östling
-        <h2>Utvecklings team</h2>
         <ul>
-          <li>Boss: Marcus Östling</li>
-          <li>Ellinor Westerbergr</li>
-          <li>Gustav Schneider</li>
+          <li>Marcus Östling</li>
         </ul>
-        <h2>Event ansvarig</h2>
+
+        <h2>Utvecklingsteam</h2>
         <ul>
-          <li>Carl Dath</li>
-          <li>Joakim Lilja</li>
+          {this.mapToList(this.state.team.developers)}
+        </ul>
+
+        <h2>Event team</h2>
+        <ul>
+          {this.mapToList(this.state.team.eventers)}
+        </ul>
+
+        <h2>Hangarounds</h2>
+        <ul>
+          {this.mapToList(this.state.team.hangarounds)}
         </ul>
       </div>
     );
